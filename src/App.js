a453ab59e9458f64cb6203e41a58d42000c37719
armin-bkh <arminbkh0921@yahoo.com>
2021-11-30 20:16:02 +0330

    prictice responsive design in react-bootstrap with use Row and Col

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import { Container } from "react-bootstrap";
-import NavB from "./Components/Nav/Nav";
-
+import { Col, Container, Row } from "react-bootstrap";
 function App() {
   return (
-    <Container>
-      <NavB />
+    <Container fluid="lg">
+      <Row>
+        <Col xs={12} md={4} lg={3}>
+          1
+        </Col>
+        <Col xs={12} md={4} lg={3}>
+          2
+        </Col>
+        <Col xs={12} md={4} lg={3}>
+          3
+        </Col>
+        <Col xs={12} md={4} lg={3}>
+          4
+        </Col>
+      </Row>
+      <Row className={`flex justify-content-center`}>
+        <Col xs={(12, { order: "last" })} md={(6, { order: "first" })} lg={3}>
+          h
+        </Col>
+        <Col xs={12} md={6} lg={3}>
+          l
+        </Col>
+        <Col xs={12} md={6} lg={3}>
+          m
+        </Col>
+      </Row>
     </Container>
   );
 }
